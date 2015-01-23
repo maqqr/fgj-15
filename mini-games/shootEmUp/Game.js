db6f939297fd74021a6165e7424fdf4a34f5b209
Tero Paavolainen <tero.s.t.paavolainen@student.jyu.fi>
2015-01-24 00:37:16 +0200

    hirveitä iffejä ja syntiä: tekstit kuntoon ja pelaajat näppäinten mukaan laudalle

--- mini-games/shootEmUp/Game.js
+++ mini-games/shootEmUp/Game.js
@@ -2,7 +2,7 @@ var jammu;
 var jimmu;
 var lvlWidth = 800;
 var lvlHeight = 600;
-var game = new Phaser.Game(lvlWidth, lvlHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game;
 var cursors;
 var speed = 200;
 //var maxScore = 5;
@@ -14,10 +14,17 @@ var player1Text;
 var player2Text;
 var amountOfSpikes = 5;
 var spikes;
-var player1StartX = 75;
-var player2StartX = 725;
+var player2StartX = 75;
+var player2StartY = 100;
+var player1StartX = 725;
+var player1StartY = 500;
 var balls = [];
 var destroyed = false;
+init();
+
+function init(){
+	game = new Phaser.Game(lvlWidth, lvlHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+}
 
 function preload()
 {
@@ -32,8 +39,8 @@ function preload()
 function create(){
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.stage.backgroundColor = '#339933';
-	jammu = createPlayer('jammu', 1, false, 75, 100);
-	jimmu = createPlayer('jimmu', 2, false, 725, 500);	
+	jammu = createPlayer('jammu', 1, false, player1StartX, player1StartY);
+	jimmu = createPlayer('jimmu', 2, false, player2StartX, player2StartY);	
 	jimmu.sprite.body.bounce.setTo(1,1);
 	jammu.sprite.body.bounce.setTo(1,1);
 	//game.physics.arcade.enable(jimmu);
@@ -44,14 +51,14 @@ function create(){
 	game.time.events.add(Phaser.Timer.SECOND * startTime, start, this);
 	createSpikes();
 	
-	player1Text = game.add.text(lvlWidth *0.1 -50, 50, "P1 has collected "+jammu.score+" balls!", { fontSize: '22px', fill: '#fff' });
-	player2Text = game.add.text(lvlWidth *0.65 -50, 50, "P2 has collected "+jimmu.score+" balls!", { fontSize: '22px', fill: '#fff' });
+	player2Text = game.add.text(lvlWidth *0.1 -50, 50, "Game on!", { fontSize: '22px', fill: '#fff' });
+	player1Text = game.add.text(lvlWidth *0.75 -50, 50, "Game on!", { fontSize: '22px', fill: '#fff' });
 	startingTextComponent = game.add.text(lvlWidth *0.2-50, lvlHeight * 0.5 -10, startingText, { fontSize: '22px', fill: '#fff' });
 }
 
 function createSpikes(){
 	for(var i = 0; i < amountOfSpikes; i++){
-		var spike = game.add.sprite(game.rnd.integerInRange(player1StartX+50, player2StartX-50), game.rnd.integerInRange(0, 600), 'spikes');
+		var spike = game.add.sprite(game.rnd.integerInRange(player1StartX-50, player2StartX+50), game.rnd.integerInRange(0, 600), 'spikes');
 		game.physics.arcade.enable(spike);
 		spike.body.immovable = true;
 		spikes[spikes.length] = spike;
@@ -106,12 +113,35 @@ function update(){
 
 
 	
-	player1Text.text = "P1 has collected "+jammu.score+" balls!";
-	player2Text.text = "P2 has collected "+jimmu.score+" balls!";
+	player1Text.text = defineText(1);
+	player2Text.text = defineText(2);
 	jimmu.update(game, balls);
 	jammu.update(game, balls);
 }
 
+
+function defineText(playerNumber){
+	var text = "Game on!";
+	if(playerNumber == 1){
+		if(jammu.score === 1){
+		text = "You have won!";
+		
+		}
+		else if( jimmu.score === 1){
+		 text = "You have lost";
+		}
+	}
+	else{
+		if(jimmu.score === 1){
+		text = "You have won!";
+		
+		}
+		else if( jammu.score === 1){
+		 text = "You have lost";
+		}
+	}
+	return text;
+}
 
 function destroy(){
 	if(destroyed) return;
