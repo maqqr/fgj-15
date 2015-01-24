99249fd5cb874a2ed05169ed6261c683677107f8
sakamiai <sakamiai@student.jyu.fi>
2015-01-24 13:50:01 +0200

    readme.md takaisin ja lisätty muutama jutskeli

--- lifecycle-manager/JavaScriptEngine/Init.js
+++ lifecycle-manager/JavaScriptEngine/Init.js
@@ -4,4 +4,10 @@
 //s.src = "http://somedomain.com/somescript";
 //$("head").append(s);
 
+$(document).ready(function(){
+   // do jQuery
 
+    $("#startGame").click(function () { 
+       $("#gameFrame").attr("src", "./../mini-games/dontHurtYourself/index.html");
+    });
+});
